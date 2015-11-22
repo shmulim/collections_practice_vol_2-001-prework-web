0ca389f60094a50d4996aec72ec704b91466c1e7
shmulim <shmuel.markel@gmail.com>
2015-11-21 21:05:38 -0800

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all? { |i| i.start_with?("r") }
+end
+
+def contain_a(arr)
+  arr.select { |i| i.include?("a") }
+end
+
+def first_wa(arr)
+  arr.find { |i| if i.is_a? String then i.start_with?("wa") end }
+end
+
+def remove_non_strings(arr)
+  arr.select { |i| i.is_a? String }
+end
